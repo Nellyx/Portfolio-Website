91a1cdd04ff7fa33a29cd2892269588f61cb9ba0
Nellyx <neilsonmike14@gmail.com>
2022-06-25 01:09:37 +0100

    work section almost complete

--- src/components/Works.js
+++ src/components/Works.js
@@ -5,21 +5,27 @@ function Works() {
   return (
     <div className="work">
       <div className="content">
-        <div>
+        <div className="container-one">
           <h1>Frontend Development</h1>
           <p>
             Websites application or other design related tasks. I love tackling
             digital problems.I build modern and scalable user interfaces using
             modern frontend web technologies.
           </p>
+
+          <button>
+            See my works
+            <img src="/images/east.svg" alt="" />
+          </button>
         </div>
 
-        <div>
+        <div className="container-two">
           <h1>UI/UX Design</h1>
           <p>
             I design too using Figma tool. I'm dedicated to providing fluid user
             experiences while being stylish.
           </p>
+          <button>See my works</button>
         </div>
       </div>
     </div>
