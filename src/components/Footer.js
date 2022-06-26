84c5240613c999b388159e2f9e451f14ba98abe7
Nellyx <neilsonmike14@gmail.com>
2022-06-26 23:23:09 +0100

    footer content commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,28 +5,33 @@ function Footer() {
   return (
     <div className="footer">
       <div className="footer-content">
+        <p className="hello-text">Say hello</p>
+
         <div className="footer-flex">
           <div>
-            <h1>Say Hello</h1>
-            <p>
+            <div className="footer-box">
               <img src="images/mail.svg" alt="" />
-              neilsonmike14
-            </p>
-            <p>
+              <p>neilsonmike14</p>
+            </div>
+
+            <div className="footer-box">
               <img src="images/call.svg" alt="" />
-              08061332918
-            </p>
-            <p>
+              <p>08061332918</p>
+            </div>
+
+            <div className="footer-box">
               <img src="images/location.svg" alt="" />
-              Abeokuta Ogunstate
-            </p>
+              <p>Abeokuta Ogunstate</p>
+            </div>
           </div>
 
-          <div>
+          <div className="link">
             <p>My work</p>
             <p>Download Resume</p>
           </div>
         </div>
+        <div className="line"></div>
+        <p>Michael Nelson 2022</p>
       </div>
     </div>
   );
