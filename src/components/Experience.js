92e52ad864d4cbbd9c042d8dd97a39e35348f8a5
Nellyx <neilsonmike14@gmail.com>
2022-07-01 16:56:36 +0100

    changes were made

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -6,24 +6,26 @@ function Experience() {
     <div className="experience">
       <div className="flex">
         <div className="text">
-          <h1 className="header-text">3+ years working experience</h1>
-          <p className="paragraph">
-            Over the years, I have builts products for large and small
-            businesses all across the country, ranging from marketing websites
-            to complex commercial websites, all with an emphasis on providing
-            fast, elegant and accesible user experiences
-          </p>
-          <p className="paragraph">
-            Currently, I work at African Trainovation Consult as a Frontend
-            Engineer where i build applications using Reactjs
-          </p>
+          <div className="left-margin">
+            <h1 className="header-text">3+ years working experience</h1>
+            <p className="paragraph">
+              Over the years, I have builts products for large and small
+              businesses all across the country, ranging from marketing websites
+              to complex commercial websites, all with an emphasis on providing
+              fast, elegant and accesible user experiences
+            </p>
+            <p className="paragraph">
+              Currently, I work at African Trainovation Consult as a Frontend
+              Engineer where i build applications using Reactjs
+            </p>
 
-          <p className="paragraph">
-            Before now, the prinicipal frontend Engineer at 720degree Innovation
-            Hub, where i built different user interfaces using modern web
-            technologies
-          </p>
-          <button>Download Resume</button>
+            <p className="paragraph">
+              Before now, the prinicipal frontend Engineer at 720degree
+              Innovation Hub, where i built different user interfaces using
+              modern web technologies
+            </p>
+            <button>Download Resume</button>
+          </div>
         </div>
         <div className="box">
           <div className="image-box">
