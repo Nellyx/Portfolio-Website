d87c348a819eb902ab3fc3630f816f25b1b52c71
Nellyx <neilsonmike14@gmail.com>
2022-07-07 16:23:44 +0100

    changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,46 +1,21 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { NavLink } from "react-router-dom";
-import "./Navbar.css";
-import { getTopnav } from "../data/Navbars";
+
 function Navbar() {
-  const [navitems, setnavitems] = useState([]);
-  const [collapse, setcollapse] = useState("nav-menu");
-
-  const [togglericon, settogglericon] = useState("toggler-icon");
-
-  useEffect(() => {
-    setnavitems(getTopnav());
-  }, []);
-
-  const onToggle = () => {
-    collapse === "nav-menu"
-      ? setcollapse("nav-collapse")
-      : setcollapse("nav-menu");
-
-    togglericon === "toggler-icon"
-      ? settogglericon("x-icon")
-      : settogglericon("toggler-icon");
-  };
-
   return (
     <div className="navbar">
-      <nav className="nav">
-        <NavLink className="brand" exact to="/">
-          nelly
-        </NavLink>
-        <ul className={collapse}>
-          {navitems.map((item) => (
-            <li key={item.id} className="nav-item">
-              <NavLink className="nav-link" to={item.to}>
-                {item.label}
-              </NavLink>
-            </li>
-          ))}
-        </ul>
-        <div className={togglericon} onClick={onToggle}>
-          <div className="line-1"></div>
-          <div className="line-2"></div>
-          <div className="line-3"></div>
+      <nav>
+        <NavLink>nelly</NavLink>
+        <div className="nav-links">
+          <NavLink className="nav-item" to="/about">
+            About
+          </NavLink>
+          <NavLink className="nav-item" to="/about">
+            Work
+          </NavLink>
+          <NavLink className="nav-item" to="/about">
+            Contact
+          </NavLink>
         </div>
       </nav>
     </div>
--- src/data/Navbars.js
+++
@@ -1,9 +0,0 @@
-const topNav = [
-  { id: 1, label: "About", to: "/about" },
-  { id: 2, label: "Work", to: "/myworks" },
-  { id: 3, label: "Contact", to: "/contactbar" }
-];
-
-export const getTopnav = () => {
-  return topNav;
-};
