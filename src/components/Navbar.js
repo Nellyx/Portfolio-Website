101d310cfc24fdbe4e231963fde4dcfde32abd82
Nellyx <neilsonmike14@gmail.com>
2022-07-08 07:19:26 +0100

    changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,24 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-import "./Navbar.css";
-
 function Navbar() {
   return (
     <div className="navbar">
-      <nav>
-        <NavLink>nelly</NavLink>
-        <div className="nav-links">
-          <NavLink className="nav-item" to="/about">
-            About
-          </NavLink>
-          <NavLink className="nav-item" to="/about">
-            Work
-          </NavLink>
-          <NavLink className="nav-item" to="/about">
-            Contact
-          </NavLink>
-        </div>
-        <div className="toggle">
-          <div className="line-1"></div>
-          <div className="line-2"></div>
-          <div className="line-3"></div>
-        </div>
+      <nav className="=nav">
+        <NavLink className="brand" exact to="/">
+          Nelly
+        </NavLink>
+
+
+
+
+
+
+
+
+
+
+        
       </nav>
     </div>
   );
