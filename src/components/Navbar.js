869d395c468f19d1169f4700832f9f5026fb4f3b
Nellyx <neilsonmike14@gmail.com>
2022-07-08 08:02:14 +0100

    changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
+import { getTopnav } from "../data/Topnav.js";
 
 function Navbar() {
+  const [navItems, setnavItems] = useState([]);
+
+  useEffect(() => {
+    setnavItems(getTopnav);
+  }, []);
+
   return (
     <div className="navbar">
       <nav className="=nav">
@@ -9,16 +16,21 @@ function Navbar() {
           Nelly
         </NavLink>
 
+        <ul className="nav-menu">
+          {navItems.map((item) => (
+            <li key={item.id} className="nav-item">
+              <NavLink className="nav-link" to={item.to}>
+                {item.label}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
 
-
-
-
-
-
-
-
-
-        
+        <div className="toggle-icon">
+          <div className="line-1"></div>
+          <div className="line-2"></div>
+          <div className="line-3"></div>
+        </div>
       </nav>
     </div>
   );
---
+++ src/data/Topnav.js
@@ -0,0 +1,9 @@
+const topNav =  [
+  { id: 1, label: "About", to: "/about" },
+  { id: 2, label: "Work", to: "/myworks" },
+  { id: 3, label: "Contact", to: "/contactbar" },
+];
+
+export const getTopnav = () => {
+  return topNav;
+};
