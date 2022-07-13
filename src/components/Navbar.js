b4af769bc508e740f48e1c73172d3a18653e3632
Nellyx <neilsonmike14@gmail.com>
2022-07-13 21:46:37 +0100

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,41 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./Navbar.css";
 
 function Navbar() {
+  const [nav, setnav] = useState("nav-menu");
+  const [toggler, settoggler] = useState("toggler-icon");
+
+  const handleNav = () => {
+    nav === "nav-menu" ? setnav("nav-menu show") : setnav("nav-menu");
+
+    toggler === "toggler-icon"
+      ? settoggler("toggler-icon toggle")
+      : settoggler("toggler-icon");
+  };
+
+  const handleClose = () => {
+    nav === "nav-menu show" ? setnav("nav-menu") : setnav("nav-menu show");
+
+    toggler === "toggler-icon toggle"
+      ? settoggler("toggler-icon")
+      : settoggler("toggler-icon toggle");
+  };
+
   return (
-    <div className="nav-wrapper">
-      <div className="container">
-        <nav>
-          <NavLink className="brand" exact to="/">
-            nelly
-          </NavLink>
+    <div className="navbar">
+      <nav>
+        <NavLink className="brand" exact to="/">
+          nelly
+        </NavLink>
 
-          <ul className="nav-menu">
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/about">
-                About
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/myworks">
-                Works
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/contactbar">
-                Contact
-              </NavLink>
-            </li>
-          </ul>
+        <ul className={nav}>
+          <span
+            className="close"
+            onClick={() => {
+              handleClose();
+            }}
+          >
+            x
+          </span>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/about">
+              About
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/myworks">
+              Works
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/contactbar">
+              Contact
+            </NavLink>
+          </li>
+        </ul>
 
-          <div className="toggle-icon">
-            <div className="line-1"></div>
-            <div className="line-2"></div>
-            <div className="line-3"></div>
-          </div>
-        </nav>
-      </div>
+        <div
+          className={toggler}
+          onClick={() => {
+            handleNav();
+          }}
+        >
+          <div className="line-1"></div>
+          <div className="line-2"></div>
+          <div className="line-3"></div>
+        </div>
+      </nav>
     </div>
   );
 }
