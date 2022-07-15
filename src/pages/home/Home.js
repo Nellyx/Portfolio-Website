7f6ec07d2a7766a61944ae23436be58582ca81fd
Nellyx <neilsonmike14@gmail.com>
2022-07-15 13:35:30 +0100

    redefine

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,10 @@
 import React from "react";
 import Hero from "../../components/Hero";
-import Aboutbar from "../../components/Aboutbar";
-import Experience from "../../components/Experience";
-import Works from "../../components/Works";
-import Contact from "../../components/Contact";
-import Footer from "../../components/Footer";
+// import Aboutbar from "../../components/Aboutbar";
+// import Experience from "../../components/Experience";
+// import Works from "../../components/Works";
+// import Contact from "../../components/Contact";
+// import Footer from "../../components/Footer";
 import Navbar from "../../components/Navbar";
 
 function Home() {
@@ -13,11 +13,11 @@ function Home() {
       <Navbar />
 
       <Hero />
-      <Aboutbar />
-      <Experience />
-      <Works />
-      <Contact />
-      <Footer />
+      {/* <Aboutbar /> */}
+      {/* <Experience /> */}
+      {/* <Works /> */}
+      {/* <Contact /> */}
+      {/* <Footer /> */}
     </div>
   );
 }
